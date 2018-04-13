a46cf1fa0e64a8b41d21c18038e7c73adb832219
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-13 09:17:33 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,7 @@ FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
 
-WHERE pledges.amount > projects.funding_goal;"
+WHERE pledges.amount >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
