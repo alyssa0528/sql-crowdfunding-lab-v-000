3c48dbd53d2b7a84ff4e276a679a5be5738816c5
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-13 08:41:50 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,10 @@ ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, projects.funding_goal
+"SELECT projects.title, projects.funding_goal AS amount_over_goal
+FROM projects 
+INNER JOIN pledges 
+ON projects.id = pledges.project_id
 WHERE SUM(pledges.amount) > projects.funding_goal"
 end
 
